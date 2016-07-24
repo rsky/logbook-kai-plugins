b72e5ed849a415b01cc7fe58ed56fbc6d7b7cae3
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2016-07-25 00:14:04 +0900

    RetrofitのファクトリメソッドからOkHttpClientのファクトリメソッドを分離

--- pushbullet/src/main/java/pushbullet/api/ServiceFactory.java
+++ pushbullet/src/main/java/pushbullet/api/ServiceFactory.java
@@ -9,22 +9,24 @@ public class ServiceFactory {
     private static final String BASE_URL = "https://api.pushbullet.com/v2/";
 
     public static PushbulletService create(String accessToken) {
-        return buildRetrofit(accessToken).create(PushbulletService.class);
+        return createRetrofit(accessToken).create(PushbulletService.class);
     }
 
-    private static Retrofit buildRetrofit(String accessToken) {
-        OkHttpClient client = new OkHttpClient.Builder()
+    private static Retrofit createRetrofit(String accessToken) {
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(createClient(accessToken))
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+    }
+
+    private static OkHttpClient createClient(String accessToken) {
+        return new OkHttpClient.Builder()
                 .addInterceptor(chain -> chain.proceed(chain.request()
                         .newBuilder()
                         .addHeader("Access-Token", accessToken)
                         .build()))
-                .build();
-
-        return new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(client)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
     }
 }
