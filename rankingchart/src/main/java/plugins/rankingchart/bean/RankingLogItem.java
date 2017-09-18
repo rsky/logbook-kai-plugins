8e48f3e20e0c5c2b2722b819a525849226623fcd
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2017-09-18 18:07:23 +0900

    OrangeSignal CSVへの依存を排除

--- rankingchart/src/main/java/plugins/rankingchart/bean/RankingLogItem.java
+++ rankingchart/src/main/java/plugins/rankingchart/bean/RankingLogItem.java
@@ -1,36 +1,18 @@
 package plugins.rankingchart.bean;
 
-import com.orangesignal.csv.annotation.CsvColumn;
-import com.orangesignal.csv.annotation.CsvEntity;
 import lombok.Data;
 
 import java.time.ZonedDateTime;
 
-@CsvEntity
 @Data
 public class RankingLogItem {
-    @CsvColumn(name = "日時")
     ZonedDateTime dateTime;
-
-    @CsvColumn(name = "1位")
     Integer rank1;
-
-    @CsvColumn(name = "5位")
     Integer rank5;
-
-    @CsvColumn(name = "20位")
     Integer rank20;
-
-    @CsvColumn(name = "100位")
     Integer rank100;
-
-    @CsvColumn(name = "500位")
     Integer rank500;
-
-    @CsvColumn(name = "戦果")
     Integer rate;
-
-    @CsvColumn(name = "順位")
     Integer rankNo;
 
     public static RankingLogItem withDateTime(ZonedDateTime date) {
