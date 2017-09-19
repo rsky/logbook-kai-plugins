45c2f787401af28c24316248a9fe53d589e9394d
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2017-09-20 03:07:56 +0900

    順位 -> 難読化済み戦果のマップから戦果係数を導出するメソッドを実装

--- rankingchart/src/main/java/plugins/rankingchart/util/Calculator.java
+++ rankingchart/src/main/java/plugins/rankingchart/util/Calculator.java
@@ -1,9 +1,13 @@
 package plugins.rankingchart.util;
 
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Calculator {
     public static final int NO_RATE = -1;
 
-    /* 順位によって変化するランキング係数 */
+    /** 順位によって変化するランキング係数 */
     private static final int[] RANKING_RATE_MAGIC_NUMBERS =
             {8931, 1201, 1156, 5061, 4569, 4732, 3779, 4568, 5695, 4619, 4912, 5669, 6586};
 
@@ -39,5 +43,43 @@ public class Calculator {
         } else {
             return NO_RATE;
         }
+    }
+
+    /**
+     * ランキング情報から戦果係数を導出する
+     *
+     * @param source 順位 -> 難読化済み戦果のマップ
+     * @return 戦果係数
+     */
+    public static int detectRateCoefficient(Map<Integer, Long> source) {
+        // 難読化された戦果を順位別係数で割り、BigIntegerに変換する
+        Set<BigInteger> set = new HashSet<>();
+        source.entrySet()
+                .stream()
+                .map(e -> e.getValue() / RANKING_RATE_MAGIC_NUMBERS[e.getKey() % 13])
+                .map(BigInteger::valueOf)
+                .forEach(set::add);
+
+        List<BigInteger> list = new ArrayList<>(set);
+        List<Integer> gcdList = new ArrayList<>();
+
+        // 全ての組み合わせに対して最大公約数を求める
+        while (list.size() > 1) {
+            BigInteger a = list.remove(0);
+            list.stream()
+                    .map(b -> b.gcd(a))
+                    .map(BigInteger::intValue)
+                    //.filter(i -> i < 100) // 今のところ[1, 99]の範囲であるようだが
+                    .forEach(gcdList::add);
+        }
+
+        // 最頻出する最大公約数が戦果係数である（たぶん）
+        return gcdList.stream()
+                .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .map(Map.Entry::getKey)
+                .orElse(0);
     }
 }
