1eebcb9e7a5e4641438b7a523e7b1d9fae753456
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2017-09-20 21:48:24 +0900

    戦果係数の求め方をリファクタリング

--- rankingchart/src/main/java/plugins/rankingchart/util/Calculator.java
+++ rankingchart/src/main/java/plugins/rankingchart/util/Calculator.java
@@ -2,7 +2,7 @@ package plugins.rankingchart.util;
 
 import java.math.BigInteger;
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Calculator {
     public static final int NO_RATE = -1;
@@ -12,20 +12,26 @@ public class Calculator {
             {8931, 1201, 1156, 5061, 4569, 4732, 3779, 4568, 5695, 4619, 4912, 5669, 6586};
 
     /**
-     * ユーザーごとに異なる戦果係数を総当たりで導出する
+     * 戦果を計算する
      *
      * @param rankNo 順位
      * @param obfuscatedRate 難読化済み戦果
-     * @param actualRate 実際の戦果
-     * @return 戦果係数
+     * @param userRateFactor 戦果係数
+     * @param strict 難読化済み戦果が係数で割り切れない場合は失敗にする
+     * @return 戦果
      */
-    public static int detectUserRateFactor(int rankNo, long obfuscatedRate, int actualRate) {
-        for (int i = 1; i < 100; i++) {
-            if (calcRate(rankNo, obfuscatedRate, i) == actualRate) {
-                return i;
+    private static int calcRate(int rankNo, long obfuscatedRate, int userRateFactor, boolean strict) {
+        int rankRateFactor = RANKING_RATE_MAGIC_NUMBERS[rankNo % 13];
+        if (strict && obfuscatedRate % rankRateFactor != 0) {
+            return NO_RATE;
+        } else if (userRateFactor > 0) {
+            if (strict && (obfuscatedRate / rankRateFactor) % userRateFactor != 0) {
+                return NO_RATE;
             }
+            return (int) (obfuscatedRate / rankRateFactor / userRateFactor - 91);
+        } else {
+            return NO_RATE;
         }
-        return 0;
     }
 
     /**
@@ -37,49 +43,63 @@ public class Calculator {
      * @return 戦果
      */
     public static int calcRate(int rankNo, long obfuscatedRate, int userRateFactor) {
-        if (userRateFactor > 0) {
-            long rate = obfuscatedRate / RANKING_RATE_MAGIC_NUMBERS[rankNo % 13] / userRateFactor - 91;
-            return (int) ((rate > 0) ? rate : obfuscatedRate);
-        } else {
-            return NO_RATE;
-        }
+        return calcRate(rankNo, obfuscatedRate, userRateFactor, false);
+    }
+
+    /**
+     * ユーザーごとに異なる戦果係数を総当たりで導出する
+     *
+     * @param rankNo 順位
+     * @param obfuscatedRate 難読化済み戦果
+     * @param actualRate 実際の戦果
+     * @return 戦果係数
+     */
+    public static int detectUserRateFactor(int rankNo, long obfuscatedRate, int actualRate) {
+        return IntStream.range(1, 100)
+                .filter(i -> calcRate(rankNo, obfuscatedRate, i, true) == actualRate)
+                .findFirst()
+                .orElse(0);
     }
 
     /**
      * ランキング情報から戦果係数を導出する
+     * 正しい値を求めるにはある程度のサンプル数が必要
      *
-     * @param source 順位 -> 難読化済み戦果のマップ
+     * @param source 順位をキー、難読化済み戦果を値とするMap
      * @return 戦果係数
      */
-    public static int detectRateCoefficient(Map<Integer, Long> source) {
+    public static int detectUserRateFactor(Map<Integer, Long> source) {
         // 難読化された戦果を順位別係数で割り、BigIntegerに変換する
-        Set<BigInteger> set = new HashSet<>();
+        List<BigInteger> list = new ArrayList<>();
         source.entrySet()
                 .stream()
+                .filter(e -> e.getValue() % RANKING_RATE_MAGIC_NUMBERS[e.getKey() % 13] == 0)
                 .map(e -> e.getValue() / RANKING_RATE_MAGIC_NUMBERS[e.getKey() % 13])
                 .map(BigInteger::valueOf)
-                .forEach(set::add);
+                .forEach(list::add);
 
-        List<BigInteger> list = new ArrayList<>(set);
-        List<Integer> gcdList = new ArrayList<>();
-
-        // 全ての組み合わせに対して最大公約数を求める
-        while (list.size() > 1) {
-            BigInteger a = list.remove(0);
-            list.stream()
-                    .map(b -> b.gcd(a))
-                    .map(BigInteger::intValue)
-                    //.filter(i -> i < 100) // 今のところ[1, 99]の範囲であるようだが
-                    .forEach(gcdList::add);
+        // 割り切れない値が含まれていた場合は異常値とみなす
+        // (順位別係数やアルゴリズムの見直しが必要になる)
+        if (list.size() != source.size()) {
+            return 0;
         }
 
-        // 最頻出する最大公約数が戦果係数である（たぶん）
-        return gcdList.stream()
-                .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
-                .entrySet()
-                .stream()
-                .max(Comparator.comparing(Map.Entry::getValue))
-                .map(Map.Entry::getKey)
-                .orElse(0);
+        list = new ArrayList<>(new HashSet<>(list));
+
+        // 1つの値に収束するまで繰り返し最大公約数を求め、残ったのが戦果係数である
+        // (通常は2-passで終わる)
+        while (list.size() > 1) {
+            Set<BigInteger> set = new HashSet<>();
+
+            // 全ての組み合わせの最大公約数セットを作る
+            while (list.size() > 1) {
+                BigInteger a = list.remove(0);
+                list.stream().map(b -> b.gcd(a)).forEach(set::add);
+            }
+
+            list = new ArrayList<>(set);
+        }
+
+        return (list.size() == 1) ? list.get(0).intValue() : 0;
     }
 }
