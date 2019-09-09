870fed086c332222b53e926a2bbf89cd31059ad2
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2019-09-09 16:06:43 +0900

    ResourceHandlerを使ってみる

--- webfrontend/src/main/java/plugins/webfrontend/server/ExampleWebServer.java
+++ webfrontend/src/main/java/plugins/webfrontend/server/ExampleWebServer.java
@@ -2,13 +2,25 @@ package plugins.webfrontend.server;
 
 import logbook.plugin.lifecycle.StartUp;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+
+import java.nio.file.Paths;
 
 public class ExampleWebServer implements StartUp {
     @Override
     public void run() {
         try {
             Server server = new Server(7777);
-            server.setHandler(new ExampleHandler());
+
+            ResourceHandler resourceHandler = new ResourceHandler();
+            String resourcesDir = Paths.get(System.getProperty("user.dir")).resolve("./resources").toString();
+            resourceHandler.setResourceBase(resourcesDir);
+            HandlerList handlerList = new HandlerList();
+            handlerList.addHandler(resourceHandler); // first!
+            handlerList.addHandler(new IndexHandler());
+
+            server.setHandler(handlerList);
             server.start();
         } catch (Exception e) {
             e.printStackTrace();
--- webfrontend/src/main/java/plugins/webfrontend/server/IndexHandler.java
+++ webfrontend/src/main/java/plugins/webfrontend/server/IndexHandler.java
@@ -8,12 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class ExampleHandler extends AbstractHandler {
+public class IndexHandler extends AbstractHandler {
     @Override
     public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
-        httpServletResponse.setContentType("text/html;charset=utf-8");
-        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
-        request.setHandled(true);
-        httpServletResponse.getWriter().println("<h1>Hello Jetty!!</h1>");
+         if (s.equals("/index.html")) {
+             httpServletResponse.setContentType("text/html;charset=utf-8");
+             httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+             request.setHandled(true);
+             httpServletResponse.getWriter().println("<h1>Hello Jetty!!</h1>");
+         }
     }
 }
